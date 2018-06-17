775a07a4c3ba03a1e39ad89d53b6946e2845a6ec
Skies <synlgp@gmail.com>
2018-06-17 00:35:34 -0300

    Subo algoritmo que calcula el determinante y test

--- test/MatrixTest.java
+++ test/MatrixTest.java
@@ -86,4 +86,91 @@ public class MatrixTest {
 
     }
 
+    @Test
+    public void determinantOfInt2x2MatrixMustBe1() {
+
+        int[][] matrix = {{1, 0},
+                          {0, 1}};
+
+        Assert.assertEquals(1,matrixService.calculateDeterminant(matrix));
+
+
+    }
+
+    @Test
+    public void firstComplementaryMinorIs1110() {
+
+        int[][] matrix = {{1,0,1},
+                          {0,1,1},
+                          {1,1,0}};
+
+        int[][] complementaryMinor = matrixService.calculateComplementaryMinor(matrix,0);
+
+        Assert.assertArrayEquals(new int[] {1,1}, complementaryMinor[0]);
+        Assert.assertArrayEquals(new int[] {1,0}, complementaryMinor[1]);
+
+    }
+
+    @Test
+    public void secondComplementaryMinorIs0110() {
+
+        int[][] matrix = {{1,0,1},
+                          {0,1,1},
+                          {1,1,0}};
+
+        int[][] complementaryMinor = matrixService.calculateComplementaryMinor(matrix,1);
+
+        Assert.assertArrayEquals(new int[] {0,1}, complementaryMinor[0]);
+        Assert.assertArrayEquals(new int[] {1,0}, complementaryMinor[1]);
+
+    }
+
+    @Test
+    public void thirdComplementaryMinorIs0111() {
+
+        int[][] matrix = {{1,0,1},
+                          {0,1,1},
+                          {1,1,0}};
+
+        int[][] complementaryMinor = matrixService.calculateComplementaryMinor(matrix,2);
+
+        Assert.assertArrayEquals(new int[] {0,1}, complementaryMinor[0]);
+        Assert.assertArrayEquals(new int[] {1,1}, complementaryMinor[1]);
+
+    }
+
+    @Test
+    public void determinantOfInt3x3MatrixMustBeMinus2() {
+
+        int[][] matrix = {{1,0,1},
+                          {0,1,1},
+                          {1,1,0}};
+
+        Assert.assertEquals(-2,matrixService.calculateDeterminant(matrix));
+
+    }
+
+    @Test
+    public void determinantOfInt4x4MatrixMustBe376() {
+
+        int[][] matrix = {{2,3,3,6},
+                          {2,3,6,7},
+                          {4,82,0,3},
+                          {2,23,2,3}};
+
+        Assert.assertEquals(376,matrixService.calculateDeterminant(matrix));
+
+    }
+
+    @Test
+    public void determinantOfDouble3x3MatrixMustBeZero() {
+
+        double[][] matrix = {{1.5,1.5,0},
+                          {3,3,0},
+                          {2.2,2.7,1}};
+
+        Assert.assertEquals(0,matrixService.calculateDeterminant(matrix), 0.0);
+
+    }
+
 }
