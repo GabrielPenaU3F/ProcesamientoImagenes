7a31390ae11e0cb2b07a1f87775d35529943fb72
Skies <synlgp@gmail.com>
2018-07-11 02:25:41 -0300

    Subo calculos de distancia y similaridad

---
+++ src/core/service/BilateralFunctionsService.java
@@ -0,0 +1,24 @@
+package core.service;
+
+import domain.XYPoint;
+
+public class BilateralFunctionsService {
+
+    public double calculateCloseness(XYPoint center, XYPoint currentPosition, double closenessSigma){
+        double distance = this.calculateDistance(center, currentPosition);
+        return Math.exp(-0.5*Math.pow(distance/closenessSigma,2));
+    }
+
+    public double calculateSimilarity(double centerValue, double currentValue, double similaritySigma) {
+        double intensityDistance = Math.abs(currentValue - centerValue);
+        return Math.exp(-0.5*Math.pow(intensityDistance/similaritySigma,2));
+    }
+
+
+    private double calculateDistance(XYPoint center, XYPoint currentPosition) {
+        double xDistance = Math.abs(currentPosition.getX() - center.getX());
+        double yDistance = Math.abs(currentPosition.getY() - center.getY());
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2)); //Teorema de Pitágoras
+    }
+
+}
